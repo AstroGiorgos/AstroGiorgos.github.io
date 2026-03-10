24d2798f0d9f5bec7600c0397d43a1cca42d2c7d
Giorgos Leloudas <giorgos@SPC-C02DR0M6PN7C.local>
2026-03-10 13:02:35 +0100

    Enable sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 import { template } from "./src/settings";
 
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind(), sitemap()],
-    site: "https://AstroGiorgos.github.io",
-    base: "/",
+  integrations: [react(), tailwind(), sitemap()],
+  site: "https://astrogiorgos.github.io",
+  base: "/",
 });
